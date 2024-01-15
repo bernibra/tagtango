091039105c52136928cbd30a3ab18ee9c46d9f45
bernibra <bernat.bramon@gmail.com>
2024-01-15 15:25:53 +0100

    Trying to update data

--- R/data.R
+++ R/data.R
@@ -3,10 +3,10 @@
 #' A subset of the 10x dataset: 10k PBMCs from a healthy donor - gene expression and cell surface protein, single cell gene expression (Single Cell Gene Expression Dataset by Cell Ranger 3.0.0)
 #' Peripheral blood mononuclear cells (PBMCs) from a healthy donor stained with TotalSeq-B antibodies.
 #'
-#' @format ## `test_sce`
-#' A SingleCellExperment with 7472 cells and 17 protein markers:
+#' @format ## `test_data`
+#' A SingleCellExperment with 7472 cells, 17 protein markers, and 33538 gene IDs:
 #' \describe{
 #'   \item{date}{November 19, 2018}
 #' }
 #' @source <https://support.10xgenomics.com/single-cell-gene-expression/datasets/3.0.0/pbmc_10k_protein_v3>
-"test_sce"
+"test_data"
