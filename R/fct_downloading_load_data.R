0fb776ca25806c9db397d3dad7ef555b24ac4e4b
bernibra <bernat.bramon@gmail.com>
2024-01-23 09:02:38 +0100

    Adding saveguards to process_data function
    This is not entirely necessary for the app, but it would be useful if someone attempted to run the function outside of what it is expected

--- R/fct_downloading_load_data.R
+++ R/fct_downloading_load_data.R
@@ -1,6 +1,6 @@
-#' load_data
+#' process_data
 #'
-#' @description A utility function for reading a processing the data as `tagtango`
+#' @description A utility function for reading a processing the data as `tagtango` does.
 #' @param filename path to data file
 #' @param data_type experiment used in the MultiAssayExperiment or data contained in logcounts in SingleCellExperiment
 #' @param left annotation in the left of the diagram
@@ -12,26 +12,70 @@
 #'
 #' @return The return value, if any, from executing the function.
 #' @export
-process_data <- function(filename, data_type, left, right, pc1_axis1, pc1_axis2, pc2_axis1, pc2_axis2){
-  data <- read_input(filename)
-  rna <- data.frame(V1 = data$dat[,pc1_axis1], V2 = data$dat[,pc1_axis2])
-  adt <- data.frame(V1 = data$dat[,pc2_axis1], V2 = data$dat[,pc2_axis2])
+process_data <- function(filename, data_type, left, right,
+                         pc1_axis1 = NULL, pc1_axis2 = NULL,
+                         pc2_axis1 = NULL, pc2_axis2 = NULL){
 
-  sce = if(!is.null(data$mae)){data$mae[[data_type]]}else if(!is.null(data$sce)){data$sce}else{NULL}
+  if(is.null(filename)){
+    data <- read_input("test_data")
+  }else{
+    data <- read_input(filename)
+    if(data$ReadError != "Valid data"){
+      stop(data$ReadError)
+    }
+  }
 
-  dat <- list(
-    dat = data$dat,
-    left = left,
-    right = right,
-    sce = sce,
-    rna_umap = rna,
-    adt_umap = adt,
-    data_type = input$data_type
-  )
+  rna <- tryCatch({
+    data.frame(V1 = data$dat[,pc1_axis1], V2 = data$dat[,pc1_axis2])
+  }, error = function(e) {
+    NULL
+  })
+
+  adt <- tryCatch({
+    adt <- data.frame(V1 = data$dat[,pc2_axis1], V2 = data$dat[,pc2_axis2])
+  }, error = function(e) {
+    NULL
+  })
+
+  sce <- tryCatch({
+    if(!is.null(data$mae)){data$mae[[data_type]]}else if(!is.null(data$sce)){data$sce}else{NULL}
+  }, error = function(e) {
+    NULL
+  })
+
+  dat <- tryCatch({
+    list(
+      dat = data$dat,
+      left = left,
+      right = right,
+      sce = sce,
+      rna_umap = rna,
+      adt_umap = adt,
+      data_type = data_type
+    )
+  }, error = function(e) {
+    NULL
+  })
+
+  if(is.null(dat)){
+    stop("There is something odd regarding the data inputed. Please refer to the app's manual and README page for specifications on the input format.")
+  }
+
+  toomanycolumns <- FALSE
+  otherproblem <- FALSE
 
   if(any(gsub("[[:punct:]]", " ", tolower(dat$data_type)) == c("adt", "antibody capture", "protein data", "antibody derived tags", "scadt"))){
 
-    dat$norm <- t(as.matrix(SingleCellExperiment::logcounts(dat$sce)))
+    dat$norm <- tryCatch({
+      t(as.matrix(SingleCellExperiment::logcounts(dat$sce)))
+    }, error = function(e) {
+      NULL
+    })
+
+    otherproblem <- ifelse(is.null(dat$norm), TRUE, otherproblem)
+    if(!otherproblem){
+      toomanycolumns <- ifelse(ncol(dat$norm)>2000, TRUE, toomanycolumns)
+    }
 
   }else if(dat$data_type == "No expression data"){
 
@@ -39,10 +83,22 @@ process_data <- function(filename, data_type, left, right, pc1_axis1, pc1_axis2,
 
   }else{
 
-    dat$norm <- dge_rna_data(dat$sce, dat$left, dat$right, numberOFgenes = 5)
+    dat$norm <- tryCatch({
+      dge_rna_data(dat$sce, dat$left, dat$right, numberOFgenes = 5)
+    }, error = function(e) {
+      NULL
+    })
 
     otherproblem <- ifelse(is.null(dat$norm), TRUE, otherproblem)
 
+  }
+
+  if(toomanycolumns){
+    stop("The expression data is entered as ADT data, but the corresponding matrix, with more than 2000 columns, looks more like RNA data. Please specify the data type correctly.")
+  }
+
+  if(otherproblem){
+    stop("There is something odd regarding the expression data inputed. Please refer to the app's manual and README page for specifications on the input format.")
   }
 
   return(
